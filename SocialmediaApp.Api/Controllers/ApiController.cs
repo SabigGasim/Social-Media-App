56a62000b858f6f758d07df8d81219cb870805e4
SabigGasim <sabgasim@gmail.com>
2022-10-19 23:20:33 +0300

    init the social media app api

---
+++ SocialmediaApp.Api/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Api.Controllers;
+internal class ApiController
+{
+}
